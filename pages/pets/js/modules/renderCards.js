4f448ae4018b05d8fc34acddcda20af3c2e0ba64
OLGA KOLIKHALOVA <92117874+KOLIKOLGA@users.noreply.github.com>
2024-09-25 18:18:29 +0300

    pagination finish

--- pages/pets/js/modules/renderCards.js
+++ pages/pets/js/modules/renderCards.js
@@ -1,8 +1,12 @@
 export const renderCards = (pets, container) => {
   const petsContainer = document.querySelector(".js-pets-list");
   const pagination = document.querySelector(".js-pagination");
-  const pageNumber = document.querySelector(".slider-button__item_number");
+  let pageNumber = document.querySelector(".slider-button__item_number");
   const btnPagination = document.querySelectorAll(".slider-button__item");
+  const arrowLeftEnd = document.querySelector(".arrow-left-end");
+  const arrowLeft = document.querySelector(".arrow-left");
+  const arrowRight = document.querySelector(".arrow-right");
+  const arrowRightEnd = document.querySelector(".arrow-right-end");
 
   let petsArray = [];
   let result = [];
@@ -73,12 +77,96 @@ export const renderCards = (pets, container) => {
       item.addEventListener("click", () => {
         pageNumber.textContent = currentPage;
         console.log(pageNumber.textContent);
+        if (item.classList.contains("arrow-left-end")) {
+          currentPage = 1;
+          pageNumber.textContent = currentPage;
+          arrowLeft.classList.add("disabled");
+          arrowLeft.classList.remove("active");
+          arrowLeftEnd.classList.add("disabled");
+          arrowLeftEnd.classList.remove("active");
+          arrowRight.classList.remove("disabled");
+          arrowRightEnd.classList.remove("disabled");
+          arrowRight.classList.add("active");
+          arrowRightEnd.classList.add("active");
+          arrowLeft.disabled = true;
+          arrowLeftEnd.disabled = true;
+          arrowRight.disabled = false;
+          arrowRightEnd.disabled = false;
+        }
+        if (item.classList.contains("arrow-left")) {
+          if (currentPage > 1) {
+            currentPage--;
+            pageNumber.textContent = currentPage;
+            arrowLeft.disabled = false;
+            arrowLeftEnd.disabled = false;
+            arrowRight.disabled = false;
+            arrowRightEnd.disabled = false;
+          }
+          if (currentPage == 1) {
+            arrowLeft.classList.add("disabled");
+            arrowLeft.classList.remove("active");
+            arrowLeftEnd.classList.add("disabled");
+            arrowLeftEnd.classList.remove("active");
+            arrowRight.classList.remove("disabled");
+            arrowRightEnd.classList.remove("disabled");
+            arrowRight.classList.add("active");
+            arrowRightEnd.classList.add("active");
+            arrowLeft.disabled = true;
+            arrowLeftEnd.disabled = true;
+            arrowRight.disabled = false;
+            arrowRightEnd.disabled = false;
+          }
+        }
+        if (item.classList.contains("arrow-right")) {
+          arrowRight.classList.remove("disabled");
+          arrowRightEnd.classList.remove("disabled");
+          arrowRight.classList.add("active");
+          arrowRightEnd.classList.add("active");
+          arrowLeft.classList.remove("disabled");
+          arrowLeft.classList.add("active");
+          arrowLeftEnd.classList.remove("disabled");
+          arrowLeftEnd.classList.add("active");
+          currentPage++;
+          pageNumber.textContent = currentPage;
+          arrowLeft.disabled = false;
+          arrowLeftEnd.disabled = false;
+          if (currentPage == pageCount) {
+            arrowRight.classList.add("disabled");
+            arrowRightEnd.classList.add("disabled");
+            arrowRight.classList.remove("active");
+            arrowRightEnd.classList.remove("active");
+            arrowLeft.classList.remove("disabled");
+            arrowLeft.classList.add("active");
+            arrowLeftEnd.classList.remove("disabled");
+            arrowLeftEnd.classList.add("active");
+            arrowRight.disabled = true;
+            arrowRightEnd.disabled = true;
+          }
+        }
+        if (item.classList.contains("arrow-right-end")) {
+          arrowLeftEnd.classList.remove("disabled");
+          arrowLeftEnd.classList.add("active");
+          arrowLeft.classList.remove("disabled");
+          arrowLeft.classList.add("active");
+          currentPage = petsArray.length / numberOfPets;
+
+          pageNumber.textContent = currentPage;
+          arrowLeft.disabled = false;
+          arrowLeftEnd.disabled = false;
+          console.log("the end");
+
+          arrowRight.classList.add("disabled");
+          arrowRightEnd.classList.add("disabled");
+          arrowRight.classList.remove("active");
+          arrowRightEnd.classList.remove("active");
+          arrowRight.disabled = true;
+          arrowRightEnd.disabled = true;
+        }
+        renderProducts(petsArray, petsContainer, petsCount, currentPage);
       });
     });
   };
 
   renderProducts(petsArray, petsContainer, petsCount, currentPage);
   renderPagination(petsArray, petsCount);
-
-  console.log(btnPagination);
 };
